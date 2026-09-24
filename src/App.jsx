b1a95dc31bf64d19603519f631902b00697c17df
Christian Fernandez Vera <xtian.fdz@hotmail.com>
2026-09-25 00:53:42 +0200

    setup react binding

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/layout/Header"
+import MainContent from "./components/layout/MainContent";
+
+
+function App() {
+  return <>
+    < Header />
+    < MainContent />
+  </>;
+}
+
+export default App;
